33b4d5e2ef21f5f8ceff372453a7be4f2f585711
nuderand <nuderebatsin.roal@gmail.com>
2015-10-30 23:42:07 +0900

    Create singupView

--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import App from './components/App.vue'
+import app from './components/app.vue'
+import welcomeView from './components/welcomeView.vue'
+import loginView from './components/loginView.vue'
+import signupView from './components/signupView.vue'
 require('./../bower_components/bootstrap/dist/css/bootstrap.css')
 require('./../bower_components/font-awesome/css/font-awesome.min.css')
+require('./index.css')
 
 Vue.use(Router)
 
 var router = new Router()
 
-router.start(App, '#app')
+router.map({
+  '/': {
+    component: welcomeView
+  },
+  '/login': {
+    component: loginView
+  },
+  '/signup': {
+    component: signupView
+  }
+})
+
+router.start(app, '#app')
 
